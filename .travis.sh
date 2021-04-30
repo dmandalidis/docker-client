7715ff88684ad1fca293570b63c6a2098af0448b
Dimitris Mandalidis <dimitris.mandalidis@gmail.com>
2021-04-30 13:07:14 +0300

    Workaround broken docker systemd service
    - fd:// not working
    - enhance info on failure

--- .travis.sh
+++ .travis.sh
@@ -24,7 +24,12 @@ case "$1" in
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
     sudo apt-get -qq update
-    sudo apt-get -q -y purge docker-ce
+    sudo apt-get -qq remove --purge docker docker-engine docker.io containerd runc
+    sudo mkdir -p /etc/systemd/system/docker.service.d/
+    sudo cp exec-override.conf /etc/systemd/system/docker.service.d/exec-override.conf
+    sudo systemctl daemon-reload
+    sudo rm -rf /etc/docker
+    sudo rm -rf /etc/default/docker
     PACKAGE_VERSION=$(sudo apt-cache madison docker-ce | grep $DOCKER_VERSION | cut -f2 -d"|" | tr -d '[:space:]')
     if [[ -z $PACKAGE_VERSION ]]; then
       echo "No candidate package with $DOCKER_VERSION was found";
@@ -41,14 +46,13 @@ case "$1" in
     echo "Contents of /etc/docker/daemon.json:"
     sudo cat /etc/docker/daemon.json || :
 
-    echo "Contents of $HOME/.dockercfg:"
-    cat $HOME/.dockercfg || :
-
-    echo "Service logs"
+    echo "Service status"
     sudo systemctl -l status docker --no-pager
 
-    ;;
+    echo "Service logs"
+    sudo journalctl -u docker --no-pager
 
+    ;;
   *)
     echo "Unknown command $1"
     exit 2
